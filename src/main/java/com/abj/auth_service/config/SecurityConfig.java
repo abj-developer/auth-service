360ad277861b41fc9a0546b3efa49837fd0722e2
ambuj <srivastavvaambuj42@gmail.com>
2026-08-19 21:36:21 +0530

    adding excpetions to security congif for swagger

--- src/main/java/com/abj/auth_service/config/SecurityConfig.java
+++ src/main/java/com/abj/auth_service/config/SecurityConfig.java
@@ -26,6 +26,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
 
